d0c9385696820e92f25c23a2afd076c30653c7ad
rlaskarb <rlaskarb20@gmail.com>
2024-10-01 23:53:36 +0900

    10/1

--- chap01-connectiontest-lecture-source/src/main/java/com/ohgiraffers/connection/Application2.java
+++ chap01-connectiontest-lecture-source/src/main/java/com/ohgiraffers/connection/Application2.java
@@ -4,31 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Application02 {
-
+public class Application2 {
     public static void main(String[] args) {
-
         String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost/employee";
         String user = "ohgiraffers";
         String password = "ohgiraffers";
 
-
         /*index  1. Connection  객체선언 */
         Connection con = null;
 
 
+
         try {
-            /*index 2. 사용할 드러이버 등록*/
+            /*index 2. 사용할 드라이버 등록*/
             Class.forName(driver);
 
-            /*index 3. 드라이버 메니저로 Connection*/
-            con = DriverManager.getConnection(url,user,password);
+            /*index 3. 드라이버 메니저로 Connection */
+            con=DriverManager.getConnection(url,user,password);
+
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
             throw new RuntimeException(e);
-            /*index 4.사용한 커넥션 자원 닫기 */
+         /*index 4. 사용한 커넥션 자원 닫기 */
         }finally{
             if(con != null){
                 try {
@@ -38,8 +37,5 @@ public class Application02 {
                 }
             }
         }
-
-
     }
-
 }
